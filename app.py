70f689d771dccf456302a5e7ec9c4e4b503fdb52
Vincent He <vincent0710@Outlook.com>
2020-10-25 00:34:19 -0400

    Added sending image support

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 import csv
 import os
-
-from flask import Flask
+from flask import Flask, send_file
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 post_per_page = 10
 
+domain = "http://localhost:5000"
 
-class HelloWorld(Resource):
+
+class PageContent(Resource):
     def get(self, page):  # Being able to access a name variable that is defined
         with open(os.path.join(os.curdir, "templates", "Scrape.csv")) as f:
             file = csv.reader(f, delimiter=',')
@@ -17,11 +18,13 @@ class HelloWorld(Resource):
             contents = []
             index = 0
             for row in file:
-                if row[0] != "Search_Tag":
+                if row[0] != "Search Tag":
                     contents.append({
                         "Search_Tag": row[0],
                         "Title": row[1],
-                        "Seller_Name": row[4]
+                        "Seller_Name": row[4],
+                        "Seler_Picure" : f"{domain}/get_image/{row[1]} 0 {row[4]}.png",
+                        "Gig_Picture" : f"{domain}/get_image/{row[4]}.png",
                     })
                     index += 1
                 if index == post_per_page:
@@ -29,8 +32,17 @@ class HelloWorld(Resource):
 
         return {"Results": contents}
 
+@app.route('/get_image/<filename>')
+def get_image(filename):
+    # This would reutrn the image
+    if filename:
+        return send_file(os.path.join(os.curdir, "templates", "Images", filename), mimetype="image/png")
+    else:
+        return "ERROR"
 
-api.add_resource(HelloWorld, "/api/<int:page>")
+
+
+api.add_resource(PageContent, "/api/<int:page>")
 
 if __name__ == "__main__":
     app.run(debug=True)
