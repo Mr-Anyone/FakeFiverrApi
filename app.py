7a73f90c395cd1abdde7623b253c8613a7738b11
Vincent He <vincent0710@Outlook.com>
2020-10-24 23:58:33 -0400

    Add parameters for the endpoint and access it

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 api = Api(app)
 
 class HelloWorld(Resource):
-    def get(self):
-        return {"data":"Hello World"}
+    def get(self, name): # Being able to access a name variable that is defined
+        return {"data" : name, "Page": "1"}
 
-api.add_resource(HelloWorld, "/helloworld") # using hello world as end point
+api.add_resource(HelloWorld, "/helloworld/<string:name>") # using hellowrold but a string parm is added
 
 if __name__ == "__main__" :
     app.run(debug=True)
