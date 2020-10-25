879bbcb241faa651143ffe2acf1842231cfa1b4a
Vincent He <vincent0710@Outlook.com>
2020-10-25 00:10:06 -0400

    First Fiverr api with page as parameter

--- app.py
+++ app.py
@@ -1,14 +1,36 @@
+import csv
+import os
+
 from flask import Flask
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
+post_per_page = 10
+
 
 class HelloWorld(Resource):
-    def get(self, name): # Being able to access a name variable that is defined
-        return {"data" : name, "Page": "1"}
+    def get(self, page):  # Being able to access a name variable that is defined
+        with open(os.path.join(os.curdir, "templates", "Scrape.csv")) as f:
+            file = csv.reader(f, delimiter=',')
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>") # using hellowrold but a string parm is added
+            contents = []
+            index = 0
+            for row in file:
+                if row[0] != "Search_Tag":
+                    contents.append({
+                        "Search_Tag": row[0],
+                        "Title": row[1],
+                        "Seller_Name": row[4]
+                    })
+                    index += 1
+                if index == post_per_page:
+                    break  # don't send to much data at once
 
-if __name__ == "__main__" :
+        return {"Results": contents}
+
+
+api.add_resource(HelloWorld, "/api/<int:page>")
+
+if __name__ == "__main__":
     app.run(debug=True)
